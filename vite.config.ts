4cb957e30464ae048b47769665f1ba9d6d9a09b3
yaty30 <josueyat318@gmail.com>
2025-04-09 02:45:54 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/yu-po-engineering",
+  base: "/yu-po-engineering/",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
