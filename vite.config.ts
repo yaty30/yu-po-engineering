7d7d2a292e3d8a0420dd5a76015ce3ffd8065312
yaty30 <josueyat318@gmail.com>
2025-04-09 02:25:51 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/yu-po-engineering/",
+  base: "/yu-po-engineering",
   resolve: {
     alias: {
       "~": "/src",
