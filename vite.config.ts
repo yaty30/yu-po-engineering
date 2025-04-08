6f68f758005db9783bbda958c923c6ea4284b471
yaty30 <josueyat318@gmail.com>
2025-04-09 02:41:16 +0800

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
-// Create dirname equivalent for ESM
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/yu-po-engineering",
   resolve: {
     alias: {
-      "~": "/src",
+      "~": path.resolve(__dirname, "./src"),
     },
   },
 });
